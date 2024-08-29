8ccb4798452698f77525ca609395e730a4328e14
Pedro Andriow <pedroa@juntoseguros.com>
2024-08-29 11:08:36 -0300

    Update testAirflow.py

--- testAirflow.py
+++ testAirflow.py
@@ -86,16 +86,14 @@ class TestAirflow(unittest.TestCase):
         self.assertTrue('Authorization' in headers)
         self.assertIsNone(cookies)
 
-    @patch('sys.exit')
     @patch('sys.stdout', new_callable=StringIO)
     @patch('requests.get')
-    def testExecuteRequest(self, mock_get, mock_stdout, mock_exit):
+    def testExecuteRequest(self, mock_get, mock_stdout):
         url = f'http://www.google.com/'
         
         error = 'HTTPError ao chamar a URL'
         mock_get.side_effect = requests.exceptions.HTTPError
         self.airflow.executeRequest('GET', url)
-        mock_exit.assert_called_once_with(1)
         self.assertTrue(error in str(mock_stdout.getvalue()))
 
         #error = 'Timeout ao chamar a URL'
